308f26d1e419497cc848eb517527a553e7ac9856
kim <yuyukgh@naver.com>
2021-12-12 04:56:30 +0900

    feat: 리뷰 글쓰기 페이지 기본 마크업 업데이트

--- src/components/review/Write.js
+++ src/components/review/Write.js
@@ -19,7 +19,52 @@ function Write() {
   return (
     <div className={cx('container')}>
       <div className={cx('limiter')}>
-        <h2>{nickName}님만의 생각을 작성해주세요</h2>
+        <h2 className={cx('title')}>
+          <span className={cx('user')}>{nickName}</span>님만의 생각을
+          작성해주세요
+        </h2>
+
+        <form>
+          <ul className={cx('signup')}>
+            <li>
+              <label>제목을 작성해주세요.</label>
+              <input
+                type="text"
+                required
+                name="title"
+                placeholder="제목 작성"
+                maxLength={100}
+              />
+            </li>
+            <li>
+              <label>영화 제목을 작성해주세요.</label>
+              <input
+                type="text"
+                required
+                name="movie"
+                placeholder="영화 제목"
+              />
+            </li>
+            <li>
+              <label>내용을 작성해주세요.</label>
+              <textarea></textarea>
+            </li>
+            <li>
+              <label className={cx('fileLabel')} htmlFor="file">
+                이미지 첨부
+              </label>
+              <input
+                id="file"
+                className={cx('file')}
+                type="file"
+                name="file"
+                accept="image/*"
+              />
+            </li>
+          </ul>
+
+          <input className={cx('submit')} type="submit" value="리뷰 등록" />
+        </form>
       </div>
     </div>
   );
