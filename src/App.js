f31b590507c66a07a41eb7a877b88e6ec2fd1487
kim <yuyukgh@naver.com>
2021-12-12 10:18:34 +0900

    chore: import 테스트

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Home from './pages/Home';
 import Review from './pages/review/Review';
 import ReviewDetail from './pages/review/Detail';
 import ReviewWrite from './pages/review/Write';
-import Login from './components/auth/Login22';
+// import Login from './components/auth/Login22';
 import Sign from './components/auth/Sign';
 
 const cx = classNames.bind(styles);
@@ -43,7 +43,7 @@ function App() {
                 ''
               ) : (
                 <>
-                  <Route path={links.login} element={<Login />} />
+                  {/* <Route path={links.login} element={<Login />} /> */}
                   <Route path={links.signin} element={<Sign />} />
                 </>
               )}
